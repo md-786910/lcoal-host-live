f4441d8959ba8591ae373acf145e7f3351103094
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-12-11 22:32:57 +0530

    fix issues

--- client/client.js
+++ client/client.js
@@ -1,20 +1,20 @@
-const socketServer = "";
-
+const socketServer = "http://localhost:3000";
 const hostToLive = "http://localhost";
-
 const socket = require("socket.io-client")(socketServer);
-
 const { SSL_OP_NO_TICKET } = require("constants");
-
 const superagent = require("superagent");
+
+console.log("Superagent");
 
 socket.on("connect", () => {
   console.log("connected");
 });
 
+console.log("Superagent12");
 socket.on("disconnect", () => {
   console.log("connection lost");
 });
+console.log("Superagent123");
 
 socket.on("page-request", (data) => {
   const path = data.pathname;
@@ -23,31 +23,90 @@ socket.on("page-request", (data) => {
 
   const localhostUrl = hostToLive + path;
 
-  if (method === "get") excureGet(localhostUrl, params);
-  else if (method === "post") excurePost(localhostUrl, params);
+  if (method === "get") {
+    excuteGet(localhostUrl, params);
+  } else if (method === "post") {
+    excutePost(localhostUrl, params);
+  }
 });
 
-function excureGet(url, params) {
+function excuteGet(url, params) {
   superagent
-    .get(url, params)
+    .get(url)
     .query(params)
     .end((err, response) => {
       if (err) {
         return console.log(err);
       }
 
-      socket.emit("page-response", response, text);
+      socket.emit("page-response", response.text); // fixed 'text' variable
     });
 }
-function excurePost(url, params) {
+
+function excutePost(url, params) {
   superagent
-    .post(url, params)
-    .query(params)
+    .post(url)
+    .send(params) // fixed sending params in the request body
     .end((err, response) => {
       if (err) {
         return console.log(err);
       }
 
-      socket.emit("page-response", response, text);
+      socket.emit("page-response", response.text); // fixed 'text' variable
     });
 }
+
+// const socketServer = "http://localhost:3000";
+
+// const hostToLive = "http://localhost";
+
+// const socket = require("socket.io-client")(socketServer);
+
+// const { SSL_OP_NO_TICKET } = require("constants");
+
+// const superagent = require("superagent");
+// console.log("Superagent");
+
+// socket.on("connect", () => {
+//   console.log("connected");
+// });
+
+// socket.on("disconnect", () => {
+//   console.log("connection lost");
+// });
+
+// socket.on("page-request", (data) => {
+//   const path = data.pathname;
+//   const method = data.method;
+//   const params = data.params;
+
+//   const localhostUrl = hostToLive + path;
+
+//   if (method === "get") excureGet(localhostUrl, params);
+//   else if (method === "post") excurePost(localhostUrl, params);
+// });
+
+// function excureGet(url, params) {
+//   superagent
+//     .get(url, params)
+//     .query(params)
+//     .end((err, response) => {
+//       if (err) {
+//         return console.log(err);
+//       }
+
+//       socket.emit("page-response", response, text);
+//     });
+// }
+// function excurePost(url, params) {
+//   superagent
+//     .post(url, params)
+//     .query(params)
+//     .end((err, response) => {
+//       if (err) {
+//         return console.log(err);
+//       }
+
+//       socket.emit("page-response", response, text);
+//     });
+// }
